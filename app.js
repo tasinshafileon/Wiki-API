f761b951330a7739820398892b916f0c2195f319
Tasin Shafi Leon <leontasinshafi@gmail.com>
2021-12-24 19:25:10 +0600

    Get, Put, Patch and Delete functionality added to the API

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -18,50 +20,104 @@ const articleSchema = {
 
 const Article = mongoose.model("article", articleSchema);
 
-app.get("/articles", function(req, res){
-  Article.find({}, function(err, found) {
-    if (!err) {
-      res.send(found);
-    }else{
-      res.send(err);
-    }
-  });
-});
-
-app.post("/articles", function(req, res){
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-  newArticle.save(function(err){
-    if(!err){
-      res.send("Article added successfully");
-    }else{
-      res.send(err);
-    }
-  });
-});
-
-app.delete("/articles", function(req, res){
-  Article.deleteMany({}, function(err){
-    if(!err){
-      res.send("Deleted all the articles successfully");
-    }else{
-      res.send(err);
-    }
-  });
-});
-
-app.get("/articles/:article", function(req, res){
-  Article.find({title: {'$regex': req.params.article, $options:'i'}}, function(err, found){
-    if(!err){
-      res.send(found);
-    }else{
-      res.send(err);
-    }
+app.route("/articles")
+  .get(function(req, res) {
+    Article.find({}, function(err, found) {
+      if (!err) {
+        res.send(found);
+      } else {
+        res.send(err);
+      }
+    });
   })
-});
+  .post(function(req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Article added successfully");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function(req, res) {
+    Article.deleteMany({}, function(err) {
+      if (!err) {
+        res.send("Deleted all the articles successfully");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
-app.listen(process.env.port || 3000 , function(){
+app.route("/articles/:article")
+  .get(function(req, res) {
+    Article.findOne({
+      title: {
+        $regex: req.params.article,
+        $options: 'i'
+      }
+    }, function(err, found) {
+      if (!err) {
+        res.send(found);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .put(function(req, res) {
+    Article.findOneAndUpdate({
+      title: {
+        $regex: req.params.article,
+        $options: 'i'
+      }
+    }, {
+      title: req.body.title,
+      content: req.body.content
+    }, {
+      overwrite: true
+    }, function(err) {
+      if (!err) {
+        res.send("Article Successfully Updated");
+      }else {
+        res.send(err);
+      }
+    });
+  })
+  .patch(function(req, res) {
+    Article.updateOne({
+      title: {
+        $regex: req.params.article,
+        $options: 'i'
+      }
+    }, {
+      $set: req.body
+    }, function(err) {
+      if (!err) {
+        res.send("Article Successfully Patched");
+      }else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function(req, res) {
+    Article.deleteOne({
+      title: {
+        $regex: req.params.article,
+        $options: 'i'
+      }
+    }, function(err){
+      if(!err){
+        res.send("Article Deleted")
+      }else {
+        res.send(err);
+      }
+    });
+  });
+
+app.listen(process.env.port || 3000, function() {
   console.log("Server is up and running");
 });
